faa2ed8ca663ea0a0ba2364c81ac27b7c5ffe229
taufiqhidayatjkt <taufiqhidayatjkt@gmail.com>
2019-04-24 10:20:18 +0700

    first commit

--- db_function/funct_org.php
+++ db_function/funct_org.php
@@ -31,12 +31,27 @@ function delOrgData($id){
   mysqli_close($conn);
 }
 
+// function editOrgData($id){
+//   $conn = connect();
+//   $query = "SELECT * FROM tb_organisasi WHERE id=".$id;
+//   $result = mysqli_query($conn,$query) or die (mysqli_error($conn));
+//   $row = mysqli_fetch_assoc($result);
+//   return $row;
+// }
+
 function editOrgData($id){
   $conn = connect();
-  $query = "SELECT * FROM tb_organisasi WHERE id=".$id;
-  $result = mysqli_query($conn,$query) or die (mysqli_error($conn));
-  $row = mysqli_fetch_assoc($result);
-  return $row;
+  $query = "SELECT * FROM tb_organisasi WHERE id=?";
+  if ($stmt = mysqli_prepare($conn,$query) or die (mysqli_error($conn))) {
+    mysqli_stmt_bind_param($stmt,"i",$id);
+    mysqli_stmt_execute($stmt) or die (mysqli_error($conn));
+    mysqli_stmt_bind_result($stmt, $returnId, $returnNama);
+    mysqli_stmt_fetch($stmt);
+    $result = array('id'=>$returnId, 'nama'=>$returnNama);
+    mysqli_stmt_close($stmt);
+  }
+  mysqli_close($conn);
+  return $result;
 }
 
 function updateOrgData($nama,$id){
